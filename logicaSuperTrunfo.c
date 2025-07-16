87b2e22148152997e6d79e064a59a34b7d199fdf
cleybson-estacio <cleybson.estacio@gmail.com>
2025-07-16 00:32:03 +0000

    Refatora a lógica de comparação de cartas e melhora a exibição do menu interativo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,69 +2,135 @@
 #include <string.h>
 
 // Função para exibir o menu e retornar a escolha
-int exibirMenu()
+// Exibe o menu principal e retorna a opção escolhida
+int exibirMenu(char titulo[], int atributoBloqueado)
 {
   int opcao;
-  printf("\n=== MENU DE COMPARAÇÃO DE CARTAS ===\n");
-  printf("1 - População\n");
-  printf("2 - Área\n");
-  printf("3 - PIB\n");
-  printf("4 - Pontos Turísticos\n");
-  printf("5 - PIB per capita\n");
-  printf("6 - Densidade Populacional (menor vence)\n");
-  printf("7 - Super Poder (soma dos atributos)\n");
-  printf("Escolha o atributo para comparar (1 a 7): ");
+  printf("\n%s\n", titulo);
+  for (int i = 1; i <= 6; i++)
+  {
+    if (i == atributoBloqueado)
+      continue;
+
+    switch (i)
+    {
+    case 1:
+      printf("1 - População\n");
+      break;
+    case 2:
+      printf("2 - Área\n");
+      break;
+    case 3:
+      printf("3 - PIB\n");
+      break;
+    case 4:
+      printf("4 - Pontos Turísticos\n");
+      break;
+    case 5:
+      printf("5 - PIB per capita\n");
+      break;
+    case 6:
+      printf("6 - Densidade Populacional\n");
+      break;
+    }
+  }
+  printf("Escolha uma opção: ");
   scanf("%d", &opcao);
   return opcao;
 }
 
-// Função de comparação entre as cartas
-// Quando o atributo for densidade, será passado 1/densidade, então quanto maior o número, menor a densidade original
-void compararCartas(
-    char cidade1[], char estado1[], float valor1,
-    char cidade2[], char estado2[], float valor2,
-    char atributo[], short int inverso)
+int escolherSegundoAtributo(int atributo1)
 {
-  printf("\nComparação de cartas (Atributo: %s):\n\n", atributo);
-  printf("Carta 1 - %s (%s): %.2f\n", cidade1, estado1, valor1);
-  printf("Carta 2 - %s (%s): %.2f\n", cidade2, estado2, valor2);
+  int opcao = exibirMenu("Escolha o segundo atributo (diferente do primeiro):", atributo1);
+  if (opcao == atributo1)
+  {
+    printf("Você já escolheu esse atributo. Escolha outro.\n");
+    return escolherSegundoAtributo(atributo1); // chamada recursiva
+  }
+  return opcao;
+}
 
-  if (inverso)
+// Retorna o nome do atributo
+char *nomeAtributo(int opcao)
+{
+  switch (opcao)
   {
-    if (valor1 < valor2)
-    {
-      printf("\nResultado: Carta 1 (%s) venceu!\n", cidade1);
-    }
-    else if (valor2 < valor1)
-    {
-      printf("\nResultado: Carta 2 (%s) venceu!\n", cidade2);
-    }
-    else
-    {
-      printf("\nResultado: Empate!\n");
-    }
+  case 1: return "População";
+  case 2: return "Área";
+  case 3: return "PIB";
+  case 4: return "Pontos Turísticos";
+  case 5: return "PIB per capita";
+  case 6: return "Densidade Populacional";
+  default: return "Atributo inválido";
   }
-  else
+}
+
+// Função de comparação entre as cartas
+void compararCartas(
+    char cidade1[], char estado1[], float valor1A, float valor1B,
+    char cidade2[], char estado2[], float valor2A, float valor2B,
+    int atributo1, int atributo2)
+{
+  if (atributo1 == 6)
   {
-    if (valor1 > valor2)
-    {
-      printf("\nResultado: Carta 1 (%s) venceu!\n", cidade1);
-    }
-    else if (valor2 > valor1)
-    {
-      printf("\nResultado: Carta 2 (%s) venceu!\n", cidade2);
-    }
-    else
-    {
-      printf("\nResultado: Empate!\n");
-    }
+    valor1A = valor1A * -1; // Inverte a densidade para comparação
+    valor2A = valor2A * -1; // Inverte a densidade para comparação
   }
+
+  if (atributo2 == 6)
+  {
+    valor1B = valor1B * -1; // Inverte a densidade para comparação
+    valor2B = valor2B * -1; // Inverte a densidade para comparação
+  }
+
+  float soma1 = valor1A + valor1B;
+  float soma2 = valor2A + valor2B;
+
+  // Exibição dos resultados
+  printf("\n===== Comparação das Cartas =====\n\n");
+
+  printf("Carta 1 - %s (%s):\n", cidade1, estado1);
+  printf("- %s: %.2f\n", nomeAtributo(atributo1), valor1A);
+  printf("- %s: %.2f\n", nomeAtributo(atributo2), valor1B);
+  printf("- Soma ponderada: %.2f\n\n", soma1);
+
+  printf("Carta 2 - %s (%s):\n", cidade2, estado2);
+  printf("- %s: %.2f\n", nomeAtributo(atributo1), valor2A);
+  printf("- %s: %.2f\n", nomeAtributo(atributo2), valor2B);
+  printf("- Soma ponderada: %.2f\n\n", soma2);
+
+  // Resultado final
+  printf("Resultado: %s\n",
+         soma1 > soma2 ? cidade1 : soma2 > soma1 ? cidade2
+                                                 : "Empate!");
 }
 
 // Função que calcula o "super poder" da carta
 float calcSuperPoder(unsigned long int populacao, float area, float pib, int qtdTurismo, float pibPerCapita, float inversoDensidade)
 {
   return (float)populacao + area + pib + qtdTurismo + pibPerCapita - inversoDensidade;
+}
+
+// Obtém o valor numérico de um atributo
+float obterValorAtributo(int opcao, unsigned long int pop, float area, float pib, int turismo, float pibPerCapita, float densidade)
+{
+  switch (opcao)
+  {
+  case 1:
+    return (float)pop;
+  case 2:
+    return area;
+  case 3:
+    return pib;
+  case 4:
+    return (float)turismo;
+  case 5:
+    return pibPerCapita;
+  case 6:
+    return densidade;
+  default:
+    return 0;
+  }
 }
 
 int main()
@@ -141,51 +207,19 @@ int main()
   pibPerCapita2 = pib2 / populacao2;
   superPoder2 = calcSuperPoder(populacao2, area2, pib2, pontosTuristicos2, pibPerCapita2, densidade2);
 
-  // Obtenção da escolha do usuário
-  int opcao = exibirMenu();
+  // Escolha dos dois atributos
+  int atributo1 = exibirMenu("Escolha o primeiro atributo para comparação:", 0);
+  int atributo2 = escolherSegundoAtributo(atributo1);
 
-  // Processamento conforme opção
-  switch (opcao)
-  {
-  case 1:
-    compararCartas(cidade1, estado1, (float)populacao1,
-                   cidade2, estado2, (float)populacao2,
-                   "População", 0);
-    break;
-  case 2:
-    compararCartas(cidade1, estado1, area1,
-                   cidade2, estado2, area2,
-                   "Área", 0);
-    break;
-  case 3:
-    compararCartas(cidade1, estado1, pib1,
-                   cidade2, estado2, pib2,
-                   "PIB", 0);
-    break;
-  case 4:
-    compararCartas(cidade1, estado1, (float)pontosTuristicos1,
-                   cidade2, estado2, (float)pontosTuristicos2,
-                   "Pontos Turísticos", 0);
-    break;
-  case 5:
-    compararCartas(cidade1, estado1, pibPerCapita1,
-                   cidade2, estado2, pibPerCapita2,
-                   "PIB per capita", 0);
-    break;
-  case 6:
-    compararCartas(cidade1, estado1, densidade1,
-                   cidade2, estado2, densidade2,
-                   "Densidade Populacional", 1);
-    break;
-  case 7:
-    compararCartas(cidade1, estado1, superPoder1,
-                   cidade2, estado2, superPoder2,
-                   "Super Poder", 0);
-    break;
-  default:
-    printf("\nOpção inválida! Encerrando o programa.\n");
-    break;
-  }
+  // Obtenção dos valores dos atributos
+  float valor1A = obterValorAtributo(atributo1, populacao1, area1, pib1, pontosTuristicos1, pibPerCapita1, densidade1);
+  float valor2A = obterValorAtributo(atributo1, populacao2, area2, pib2, pontosTuristicos2, pibPerCapita2, densidade2);
 
+  float valor1B = obterValorAtributo(atributo2, populacao1, area1, pib1, pontosTuristicos1, pibPerCapita1, densidade1);
+  float valor2B = obterValorAtributo(atributo2, populacao2, area2, pib2, pontosTuristicos2, pibPerCapita2, densidade2);
+
+  compararCartas(cidade1, estado1, valor1A, valor1B,
+                 cidade2, estado2, valor2A, valor2B,
+                 atributo1, atributo2);
   return 0;
 }
